d9231778904a1d278ec4c9e45a056bea58fe0e84
Dynezel <dylan50xd@gmail.com>
2025-06-08 23:53:44 -0300

    Cambiosx !!!

--- config/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-//axios config
-const instance = axios.create({
-    baseURL:"http://localhost:8080", 
-    withCredentials: true, //Incluir cookies en las solicitudes
-})
-
-export default instance;
