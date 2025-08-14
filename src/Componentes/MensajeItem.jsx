698eaa1712e768c4bcedf0f42b093b14277d8db7
Dynezel <dylan50xd@gmail.com>
2025-08-14 19:26:27 -0300

    Agregado CSS a MensajeItem

--- src/Componentes/MensajeItem.jsx
+++ src/Componentes/MensajeItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../css/Mensajeria.css";
 
 const MensajeItem = React.memo(function MensajeItem({ mensaje, esActual, imagenUsuario, imagenOtro, mostrarFecha, fechaTexto, horaTexto }) {
   return (
